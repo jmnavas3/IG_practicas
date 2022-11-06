3d5e218b6419276d3bcc9e7a99292cdc2a4f99bf
jmnavas <joselillo.sk8@gmail.com>
2022-11-07 00:34:25 +0100

    P3: material (operator=,copy constructor,default_values)

--- material.cc
+++ material.cc
@@ -1,5 +1,11 @@
 #include "material.h"
 
+//** Para este archivo se ha usado la herramienta: Doxygen Documentation Generator
+
+/**
+ * @brief Constructor por defecto
+ * 
+ */
 Material::Material() {
     difuso = amb_dif_defecto;
     especular = esp_defecto;
@@ -7,10 +13,42 @@ Material::Material() {
     brillo = brillo_defecto;
 }
 
+/**
+ * @brief Constructor de copia
+ * 
+ * @param m 
+ */
+Material::Material (const Material &m) {
+    *this = m;
+}
+
+/**
+ * @brief Sobrecarga del operador de asignación
+ * 
+ * @param m 
+ * @return Material& 
+ */
+Material &Material::operator =(const Material &m){
+    difuso = m.difuso;
+    especular = m.especular;
+    ambiente = m.ambiente;
+    brillo = m.brillo;
+
+    return *this;
+}
+
+/**
+ * @brief Constructor del diagrama de clases de la P3
+ * 
+ * @param mdifuso RGBA
+ * @param mespecular RGBA
+ * @param mambiente RGBA
+ * @param brillo valor real entre 0 y 128
+ */
 Material::Material ( Tupla4f mdifuso,
-                   Tupla4f mespecular=esp_defecto,
-                   Tupla4f mambiente=amb_dif_defecto,
-                   float brillo=brillo_defecto )
+                   Tupla4f mespecular,
+                   Tupla4f mambiente,
+                   float brillo )
 {
     difuso = mdifuso;
     especular = mespecular;
@@ -19,14 +57,17 @@ Material::Material ( Tupla4f mdifuso,
     if (brillo <= 128.0f && brillo >= 0.0f) this->brillo = brillo;
 }
 
-// -----------------------------------------------------------------------------
-// Aplica la reflectividad y brillo de un material a una malla, llamada desde Malla3D:Draw 
+/**
+ * @brief Aplica la reflectividad y brillo de un material
+ * @cite Malla3D::draw
+ * 
+ */
 void Material::aplicar () {
 
-    glMaterialfv(GL_FRONT,GL_SPECULAR,ambiente);
-    glMaterialfv (GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, amb_dif_defecto);
-    // glMaterialfv(GL_FRONT,GL_AMBIENT,difuso);
-    // glMaterialfv(GL_FRONT,GL_DIFFUSE,especular);
+    glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,especular);
+    // glMaterialfv (GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, amb_dif_defecto);
+    glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT,ambiente);
+    glMaterialfv(GL_FRONT,GL_DIFFUSE,difuso);
 
 
     glMaterialf(GL_FRONT,GL_SHININESS,brillo);
--- material.h
+++ material.h
@@ -2,14 +2,14 @@
 #define MATERIAL_H_INCLUDED
 
 #include "auxiliar.h"
-#include "malla.h"
 
 class Material
 {
     private:
-        static const Tupla4f amb_dif_defecto(0.1,0.5,0.8,1.0);
-        static const Tupla4f esp_defecto(0.0,0.0,0.0,1.0);
-        static const float brillo_defecto = 40.0f;
+        const Tupla4f amb_dif_defecto = {0.1, 0.5, 0.8, 1.0};
+        const Tupla4f esp_defecto = {0.3,0.3,0.3,1.0};
+        const Tupla4f dif_defecto = {0.0,0.0,0.0,1.0};
+        const float brillo_defecto = 20.0f;
         // Tuplas RGBA, A=1
         Tupla4f difuso;
         Tupla4f especular;
@@ -17,6 +17,8 @@ class Material
         float brillo;
     public:
         Material ();
+        Material (const Material &m);
+        Material &operator=(const Material &m);
         Material ( Tupla4f mdifuso,
                    Tupla4f mespecular,
                    Tupla4f mambiente,
