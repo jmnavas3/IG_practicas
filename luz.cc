6e636493ab9f494502e622aa7f4375c041a674cb
jmnavas3 <joselillo.sk8@gmail.com>
2022-11-06 18:56:18 +0100

    P3: luz (no activar panel local aqui)

--- luz.cc
+++ luz.cc
@@ -12,6 +12,6 @@ void Luz::activar() {
     // Definir y "dibujar" la posicion de la luz
     glLightfv(id,GL_POSITION,posicion);
 
-    glEnable(GL_LIGHTING);  // activar modelo de iluminacion local
+    // glEnable(GL_LIGHTING);  // activar modelo de iluminacion local
     glEnable(id);           // activar fuente de luz "GL_LIGHTid"
 }
